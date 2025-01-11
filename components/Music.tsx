94bd367cfe6a0c35fb5ae830c91680732646ed65
ovodo <ovdizzle@gmail.com>
2025-01-11 19:08:37 +0100

    adjusted image height for mobile

--- components/Music.tsx
+++ components/Music.tsx
@@ -5,7 +5,7 @@ const Music = () => {
   return (
     <section className='w-full flex items-center'>
       <div className='text-primary w-1/2 flex gap-2 md:gap-4 flex-col'>
-        <div className='w-[100px] h-[100px] relative'>
+        <div className='w-12 h-12 md:w-[100px] md:h-[100px] relative'>
           <ViewCanvs textureUrl='/dizz.png' />
         </div>
         <h1 className='text-primary   sm:text-[48px] md:text-[56px] max-w-[656px] leading-[1.1] sm:leading-none font-bold'>
--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -12,7 +12,7 @@ const SkillCard = ({
 }) => {
   return (
     <div className='text-primary w-[362px] h-[220px]  sm:h-[268px] flex gap-2 md:gap-4 flex-col'>
-      <div className='w-[100px] h-[100px] relative'>
+      <div className='w-12 h-12 md:w-[100px] md:h-[100px] relative'>
         <ViewCanvas textureUrl={src} />
       </div>
       <h3 className='text-2xl sm:text-2xl md:text-3xl   font-semibold'>
